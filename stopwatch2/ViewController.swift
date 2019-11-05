5484059f4520898d4635390e26ed8328100ddae4
wakanyan <kyoto.kobe.1126.yuto@gmail.com>
2019-11-06 01:25:37 +0900

    var2

--- stopwatch2/ViewController.swift
+++ stopwatch2/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var label: UILabel!
+    
+    @IBOutlet var hanteiLabel: UILabel!
     
     var count: Float = 0.0
     var timer: Timer = Timer()
@@ -19,25 +21,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     @IBAction func start() {
-        timer = Timer.scheduledTimer(timeInterval: 0.01,
-                                            target: self,
-                                            selector: #selector(self.up),
-                                            userInfo: nil,
-                                            repeats: true
-                   )
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true
+            )
+        }
+        hanteiLabel.text = ""
     }
     
     @IBAction func stop() {
         if timer.isValid {
-                   timer.invalidate()
-               }
+            timer.invalidate()
+        }
+        self.hantei()
     }
-    @objc func up(){
-           count = count + 0.01
-           label.text = String(format: "%.2f", count)
-       }
+    
+    
+    @IBAction func reset() {
+        timer.invalidate() //無効にする
+        count = 0.0 //0.0を代入
+        label.text = String(format: "%.2f", count)
+    }
+    
+    
+    @objc func up() {  //メソッド
+        count = count + 0.01
+        label.text = String(format: "%.2f", count)
+    }
+    
+    @objc func hantei() {
+        if count >= 9.80 && count <= 10.20 {
+            hanteiLabel.text = "PERFECT!"
+        }else if count >= 9.70 && count <= 10.30{
+            hanteiLabel.text = "GREAT!"
+        }else if count >= 9.60 && count <= 10.40{
+            hanteiLabel.text = "GOOD!"
+        }else if count >= 9.50 && count <= 10.50{
+            hanteiLabel.text = "BAD..."
+        }
+    }
 }
 
